4b0ca39e9414fd3f55eff7310e899dfaafc9e111
Morikiyo Inoha <morikiyo.inoha@gmail.com>
2019-03-19 16:48:05 +0900

    refactor generate_csv.js

--- js/generate_csv.js
+++ js/generate_csv.js
@@ -4,15 +4,48 @@ console.log('my javascript loaded.');
   "use strict";
   console.log('my anonymous function loaded.');
 
-  let appId = null;
-  let records = null;
+  class CSV {
+    constructor(appId, records) {
+      this.appId = appId;
+      this.records = records;
+    }
 
-  function outputCSV() {
-    let rows = [];
-    let recordIds = [];
-    for (let i = 0; i < records.length; i++) {
-      const record = records[i];
-      const row = [
+    output() {
+      if (this.records.length === 0) {
+        alert('出力データがありません。')
+        return;
+      }
+
+      this._downloadFile();
+      this._updateRecords();
+
+      location.reload();
+    }
+
+    _downloadFile() {
+      const csvText = this._toCSVText();
+      const blob    = new Blob([csvText], {'type': 'text/csv'});
+      const url     = window.URL || window.webkitURL;
+      const blobURL = url.createObjectURL(blob);
+
+      let a = document.createElement('a');
+      a.download = decodeURI('output.csv');
+      a.href = blobURL;
+      a.type = 'text/csv';
+
+      a.click();
+    }
+
+    _toCSVText() {
+      let rows = [];
+      for (let i = 0; i < this.records.length; i++) {
+        rows.push(this._toCSVLine(this.records[i]));
+      }
+      return rows.join("\r\n");
+    }
+
+    _toCSVLine(record) {
+      return [
         record.expenses_account.value,
         record.description.value,
         record.amount.value,
@@ -20,73 +53,54 @@ console.log('my javascript loaded.');
         record.date.value,
         record.name.value,
         record.payment_due_date.value
-      ];
-      rows.push(row.join(','));
-      recordIds.push(record['レコード番号'].value);
+      ].join(',');
     }
 
-    if (rows.length === 0) {
-      alert('出力データがありません。')
-      return;
-    }
-
-    const csvText = rows.join("\r\n");
-    const blob    = new Blob([csvText], {'type': 'text/csv'});
-    const url     = window.URL || window.webkitURL;
-    const blobURL = url.createObjectURL(blob);
-
-    let a = document.createElement('a');
-    a.download = decodeURI('output.csv');
-    a.href = blobURL;
-    a.type = 'text/csv';
-
-    a.click();
-
-    let updateRecords = [];
-    for (let i = 0; i < recordIds.length; i++) {
-      const o = {
-        'id': recordIds[i],
-        'record': {
-          'output_csv': {
-            'value': ['済']
+    _updateRecords() {
+      let updateRecords = [];
+      for (let i = 0; i < this.records.length; i++) {
+        const o = {
+          'id': this.records[i]['レコード番号'].value,
+          'record': {
+            'output_csv': {
+              'value': ['済']
+            }
           }
         }
+        updateRecords.push(o);
       }
-      updateRecords.push(o);
-    }
 
-    let params = {
-      app: appId,
-      records: updateRecords
-    }
-
-    kintone.api('/k/v1/records', 'PUT', params, function (resp){
-      alert('レコード更新成功');
-    }, function(rest) {
-      var errmsg = 'レコード更新時にエラーが発生しました。';
-      if (resp.message !== undefined) {
-          errmsg += '\n' + resp.message;
+      let params = {
+        app: this.appId,
+        records: updateRecords
       }
-      alert(errmsg);
-    });
 
-    location.reload();
+      kintone.api('/k/v1/records', 'PUT', params, function (resp){
+        alert('レコード更新成功');
+      }, function(rest) {
+        var errmsg = 'レコード更新時にエラーが発生しました。';
+        if (resp.message !== undefined) {
+            errmsg += '\n' + resp.message;
+        }
+        alert(errmsg);
+      });
+    }
   }
 
-  function displayOutputCSVButton() {
+  function displayOutputCSVButton(csv) {
     // 増殖バグを防ぐ
-    if (document.getElementById('my_index_button') !== null) {
+    if (document.getElementById('my_output_csv_button') !== null) {
         return;
     }
 
     // ボタン
     var myIndexButton = document.createElement('button');
-    myIndexButton.id = 'my_index_button';
+    myIndexButton.id = 'my_output_csv_button';
     myIndexButton.innerText = 'CSV出力';
     myIndexButton.onclick = function() {
       if (typeof Blob !== undefined) {
-        if (window.confirm('CSVを出力します')) {
-          outputCSV();
+        if (csv && window.confirm('CSVを出力します。')) {
+          csv.output();
         }
       } else {
         window.alert('このブラウザには対応していません');
@@ -99,9 +113,8 @@ console.log('my javascript loaded.');
 
   kintone.events.on('app.record.index.show', function(event) {
     if (event.viewId === 5299939) {
-      appId = event.appId;
-      records = event.records;
-      displayOutputCSVButton();
+      const csv = new CSV(event.appId, event.records);
+      displayOutputCSVButton(csv);
     }
   });
 })();
